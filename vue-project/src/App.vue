03e9b055bbe2f43fe357e0cfd6aed8b9c303d86b
Laxsan24 <p.laxsan-2004@hotmail.com>
2025-10-16 20:12:48 +0100

    Full stack

---
+++ vue-project/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// const ordervalue = document.getElementById('ordervalue');
+// const app = document.getElementById('app');
+import {ref} from 'vue'
+const fruits = ref(['Apple', 'Banana'])
+
+    function addFruit(){
+        fruits.value.push('Cherry')
+    }
+</script> 
+<!-- Javascript (vue.js)-->
+ 
+
+<template>
+<!-- <p id = "ordervalue">Hi and welcome to my book shop!!!</p>
+    <div id="app"></div>
+     <h7 v-for="subject" in "subjects"">Subject: Math</h7><br> 
+    <h7>Location: London</h7> <br>
+    <h7>Price: £10</h7><br>
+    <h7>Spaces left: 5 spaces</h7><br>
+    To structure this do interpalation. Shouldn't do it like this.
+    <img src="/images/Math image.jpg" alt="Math book image"><br>
+    <button>
+    <h4>Remove</h4>
+    </button> -->
+
+    <div>
+        <ul>
+            <li v-for = "fruit in fruits"> {{ fruit }}
+            </li>
+        </ul>
+        <button @click="addFruit">Add fruit</button>
+    </div>
+
+    <div>
+        <h7 v-for ="Subject in Subjects"> {{ Subjects }} </h7>
+        
+
+
+    </div>
+</template>
+<!-- Used for html-->
+
+<style scoped></style> <!-- Used for css-->
---
+++ vue-project/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//This code wouldn't work in script in the vue file.
+
+createApp(App).mount('#app')
+    var webstore = new Vue({
+    el: '#app', // <=== Don't forget this comma
+    data: { // the 'data' option
+// the key 'sitename' matches the value of 'v-text' earlier
+    sitename: 'Vue.js Pet Depot'
+    }
+    });
+
+//Use
+
+//I need to know how to connect it to method data
+//Data, method and different sections of vue instance
+//Create new methods in vue js
+//how to create functions or data in vue
+//Look at vue instance (week 1c slides)
